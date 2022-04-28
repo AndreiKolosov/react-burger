e14a77d88429d2f02e424246042044ffa8911daf
Andrei Kolosov <Oldliberty@yandex.ru>
2022-04-28 20:36:06 +0200

    feat/add AppHeader

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  Logo,
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './app-header.module.css';
+
+const AppHeader = () => {
+  return (
+    <header className={`${styles.header} pt-4 pb-4`}>
+      <nav className={`${styles.navigation}`}>
+        <div className={`${styles.container}`}>
+          <menu className={`${styles.menuList} pt-4 pb-4`}>
+            <li className='pt-4 pr-5 pb-4 pl-5'>
+              <a href='/#' className={`${styles.linkActive} text text_type_main-default`}>
+                <BurgerIcon />
+                <span className='ml-2'>Конструктор</span>
+              </a>
+            </li>
+            <li className='pt-4 pr-5 pb-4 pl-5 ml-2'>
+              <a href='/#' className={`${styles.link} text text_type_main-default`}>
+                <ListIcon />
+                <span className='ml-2'>Лента заказов</span>
+              </a>
+            </li>
+          </menu>
+          <div className='pt-2 pb-2'>
+            <Logo />
+          </div>
+        </div>
+        <a href='/#' className={`${styles.link} text text_type_main-default`}>
+          <ProfileIcon type='secondary' />
+          <span className='ml-2'>Личный кабинет</span>
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default AppHeader;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/App';
+import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
-import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Button type='primary' size='small'>
-      Я кнопка из UI системы Yandex
-    </Button>
   </React.StrictMode>,
   document.getElementById('root')
 );
