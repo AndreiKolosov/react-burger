b78c86fd01c6d62c7b755cd44c6bbd2472ee6a84
Andrei Kolosov <Oldliberty@yandex.ru>
2022-08-09 19:48:00 +0200

    refactor: typed order feed

--- src/components/orders-list/orders-list.props.ts
+++ src/components/orders-list/orders-list.props.ts
@@ -1,5 +1,5 @@
 import { DetailedHTMLProps, HTMLAttributes } from 'react';
 
 export interface IOrdersList extends DetailedHTMLProps<HTMLAttributes<HTMLUListElement>, HTMLUListElement> {
-  personal: boolean;
+  personal?: boolean;
 }
--- src/pages/order-feed/order-feed.tsx
+++ src/pages/order-feed/order-feed.tsx
@@ -1,16 +1,17 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { FC, useCallback, useEffect } from 'react';
 import styles from './order-feed.module.css';
 import OrdersList from '../../components/orders-list/orders-list';
 import OrderStatistic from '../../components/order-statistic/order-statistic';
-import { useDispatch, useSelector } from 'react-redux';
 import { wsClose, wsInit, wsResetError } from '../../services/actions/ws';
 import Loader from '../../components/loader/loader';
 import Notification from '../../components/notification/notification';
+import { IOrderFeed } from './order-feed.props';
+import { useAppDispatch, useAppSelector } from '../../services/store';
 
-const OrderFeed = () => {
-  const dispatch = useDispatch();
+const OrderFeed: FC<IOrderFeed> = () => {
+  const dispatch = useAppDispatch();
 
-  const { orders, wsRequest, wsFailed } = useSelector((store) => store.ws);
+  const { orders, wsRequest, wsFailed } = useAppSelector((store) => store.ws);
 
   const resetError = useCallback(() => {
     dispatch(wsResetError());
@@ -27,18 +28,14 @@ const OrderFeed = () => {
       {wsRequest && !wsFailed && <Loader />}
       {!wsRequest && !wsFailed && orders && (
         <section className={styles.feed}>
-          <h2 className={`${styles.feed__title} text text_type_main-large mt-10 mb-5 ml-2`}>
-            Лента заказов
-          </h2>
+          <h2 className={`${styles.feed__title} text text_type_main-large mt-10 mb-5 ml-2`}>Лента заказов</h2>
           <div className={styles.feed__content}>
             <OrdersList />
             <OrderStatistic />
           </div>
         </section>
       )}
-      {wsFailed && !orders && (
-        <Notification heading='Не удалось загрузить данные...' canGoHome onClose={resetError} />
-      )}
+      {wsFailed && !orders && <Notification heading="Не удалось загрузить данные..." canGoHome onClose={resetError} />}
     </main>
   );
 };
