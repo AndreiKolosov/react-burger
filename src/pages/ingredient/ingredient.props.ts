35001725d935da16e702deb3d8c6959962591f51
Andrei Kolosov <Oldliberty@yandex.ru>
2022-08-09 19:52:56 +0200

    refactor: typed ingredient page

---
+++ src/pages/ingredient/ingredient.props.ts
@@ -0,0 +1,3 @@
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+export interface IIngredientPage extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {}
--- src/pages/ingredient/ingredient.tsx
+++ src/pages/ingredient/ingredient.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { FC } from 'react';
 import IngredientDetails from '../../components/ingredient-details/ingredient-details';
 import styles from './ingredient.module.css';
+import { IIngredientPage } from './ingredient.props';
 
-const IngredientPage = () => {
+const IngredientPage: FC<IIngredientPage> = () => {
   return (
     <main className={styles.content}>
       <section className={styles.ingredient}>
-        <h2 className={`${styles.ingredientTitle} text text_type_main-large`}>
-          Детали ингредиента
-        </h2>
+        <h2 className={`${styles.ingredientTitle} text text_type_main-large`}>Детали ингредиента</h2>
         <IngredientDetails />
       </section>
     </main>
