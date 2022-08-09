8d0b18de12c921a07b3f41c3a2b3a2702e9efca0
Andrei Kolosov <Oldliberty@yandex.ru>
2022-08-09 19:55:27 +0200

    refactor: typed home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
+import React, { FC } from 'react';
 import styles from './home.module.css';
-import { useDispatch, useSelector } from 'react-redux';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import Loader from '../../components/loader/loader';
 import BurgerConstructor from '../../components/burger-constructor/burger-constructor';
 import BurgerIngredients from '../../components/burger-ingredients/burger-ingredients';
-import { resetOrderError } from '../../services/actions/order';
-import { resetIngredientsError } from '../../services/actions/ingredients';
 import Notification from '../../components/notification/notification';
+import { IHomePage } from './home.props';
+import { useAppSelector } from '../../services/store';
 
-const HomePage = () => {
-  const { ingredientsRequest, ingredientsFailed } = useSelector((store) => store.ingredients);
-  const { registerSuccess, user } = useSelector((store) => store.user);
-  const { orderFailed } = useSelector((store) => store.order);
-
-  const dispatch = useDispatch();
-
-  const resetErrors = () => {
-    dispatch(resetIngredientsError());
-    dispatch(resetOrderError());
-  };
+const HomePage: FC<IHomePage> = () => {
+  const { ingredientsRequest, ingredientsFailed } = useAppSelector((store) => store.ingredients);
+  const { registerSuccess, user } = useAppSelector((store) => store.user);
+  const { orderFailed } = useAppSelector((store) => store.order);
 
   return (
     <div className={styles.home}>
@@ -38,7 +30,9 @@ const HomePage = () => {
       {ingredientsFailed && orderFailed && (
         <Notification message="Сервер не ответил на запрос и не дал вам ингредиенты. Возможно вы ему не нравитесь..." />
       )}
-      {registerSuccess && <Notification heading="Поздравляем!" message={`${user.name}, Вы успешно зарегестрированы!`} />}
+      {registerSuccess && user && (
+        <Notification heading="Поздравляем!" message={`${user.name}, Вы успешно зарегестрированы!`} />
+      )}
     </div>
   );
 };
