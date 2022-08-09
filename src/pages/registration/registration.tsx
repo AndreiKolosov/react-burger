7acd87e73030ba784ea5ef183fcdfa0c6b29dd72
Andrei Kolosov <Oldliberty@yandex.ru>
2022-08-09 19:27:56 +0200

    refactor: typed registartion page

--- src/pages/registration/registration.tsx
+++ src/pages/registration/registration.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import { Redirect, useLocation } from 'react-router-dom';
 import styles from './registration.module.css';
 import Form from '../../components/form/form';
@@ -6,30 +6,29 @@ import FormPrompt from '../../components/form/components/form-prompt/form-prompt
 import InputContainer from '../../components/form/components/input-container/input-container';
 import SubmitButton from '../../components/form/components/submit-btn/submit-btn';
 import { Input, PasswordInput } from '@ya.praktikum/react-developer-burger-ui-components';
-import { useDispatch, useSelector } from 'react-redux';
 import { createNewUser, resetRegisterError } from '../../services/actions/user';
 import { emailRegExp } from '../../utils/validate';
 import Loader from '../../components/loader/loader';
 import Notification from '../../components/notification/notification';
+import { IRegistration } from './registration.props';
+import { useAppDispatch, useAppSelector } from '../../services/store';
 
-const RegistrationPage = () => {
-  const { user, errMessage, registerUserRequest, registerUserErr } = useSelector(
-    (store) => store.user
-  );
+const RegistrationPage: FC<IRegistration> = () => {
+  const { user, errMessage, registerUserRequest, registerUserErr } = useAppSelector((store) => store.user);
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [hasEmailErr, setHasEmailErr] = useState(false);
-  const dispatch = useDispatch();
-  const location = useLocation();
+  const dispatch = useAppDispatch();
+  const location = useLocation<{ from: string }>();
 
   const validateEmail = useCallback(() => {
     email && setHasEmailErr(!emailRegExp.test(email));
   }, [email]);
 
-  const onEmailFocus = useCallback(() => {
+  const onEmailFocus = () => {
     setHasEmailErr(false);
-  });
+  };
 
   const handleSubmit = useCallback(
     (e) => {
@@ -51,27 +50,27 @@ const RegistrationPage = () => {
     <main className={styles.content}>
       {registerUserRequest && !registerUserErr && <Loader />}
       {!registerUserRequest && !registerUserErr && (
-        <Form title='Регистрация' name='registration' onSubmit={handleSubmit}>
-          <InputContainer gap='mb-6'>
+        <Form title="Регистрация" name="registration" onSubmit={handleSubmit}>
+          <InputContainer gap="mb-6">
             <Input
-              name='userName'
+              name="userName"
               value={userName}
-              type='text'
-              size='default'
-              placeholder='Имя'
+              type="text"
+              size="default"
+              placeholder="Имя"
               onChange={(e) => {
                 setUserName(e.target.value);
               }}
             />
           </InputContainer>
-          <InputContainer gap='mb-6'>
+          <InputContainer gap="mb-6">
             <Input
-              name='email'
+              name="email"
               value={email}
-              type='text'
-              size='default'
-              placeholder='E-mail'
-              errorText='Некорректный email'
+              type="text"
+              size="default"
+              placeholder="E-mail"
+              errorText="Некорректный email"
               error={hasEmailErr}
               onBlur={validateEmail}
               onFocus={onEmailFocus}
@@ -80,32 +79,27 @@ const RegistrationPage = () => {
               }}
             />
           </InputContainer>
-          <InputContainer gap='mb-6'>
+          <InputContainer gap="mb-6">
             <PasswordInput
-              name='password'
+              name="password"
               value={password}
-              size='default'
+              size="default"
               onChange={(e) => {
                 setPassword(e.target.value);
               }}
             />
           </InputContainer>
           <SubmitButton
-            title='Зарегистрироваться'
-            name='registration'
-            gap='mb-20'
+            title="Зарегистрироваться"
+            name="registration"
+            gap="mb-20"
             disabled={userName && email && password && !hasEmailErr ? false : true}
           />
-          <FormPrompt link='/login' prompt='Уже зарегистрированы?' linkCaption='Войти' />
+          <FormPrompt link="/login" prompt="Уже зарегистрированы?" linkCaption="Войти" />
         </Form>
       )}
       {!registerUserRequest && registerUserErr && (
-        <Notification
-          heading='Что-то пошло не так... :('
-          message={errMessage}
-          onClose={resetError}
-          canGoHome
-        />
+        <Notification heading="Что-то пошло не так... :(" message={errMessage} onClose={resetError} canGoHome />
       )}
     </main>
   );
